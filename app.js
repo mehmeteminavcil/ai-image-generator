68cb2b957d54c1c67043ee438055aec3b5689d3d
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-01-27 01:12:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const generateForm = document.querySelector(".generate-form")
+const imageGallery = document.querySelector('.image-gallery')
+const generateBtn = generateForm.querySelector(".generate-btn")
+
+
+const OPENAI_API_KEY = "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"
+
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const downloadBtn = imgCard.querySelector(".download-btn");
+    
+   
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
+    
+    
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    }
+  });
+}
+
+const generateAiImages = async (userPrompt, userImgQuantity,userImgSize) => {
+  try {
+    
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: userPrompt,
+        n: userImgQuantity,
+        size: userImgSize,
+        response_format: "b64_json"
+      }),
+    });
+
+    // Throw an error message if the API response is unsuccessful
+    if(!response.ok) throw new Error("Failed to generate AI images. Make sure your API key is valid.");
+
+    const { data } = await response.json(); // Get data from the response
+    updateImageCard([...data]);
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    generateBtn.removeAttribute("disabled");
+    generateBtn.innerText = "Generate";
+    isImageGenerating = false;
+  }
+}
+
+const handleImageGeneration = (e) => {
+  e.preventDefault();
+  if(isImageGenerating) return;
+
+ 
+  const userPrompt = e.srcElement[0].value;
+  const userImgQuantity = parseInt(e.srcElement[1].value);
+  const userImgSize = e.srcElement[2].value;
+  
+  
+  generateBtn.setAttribute("disabled", true);
+  generateBtn.innerText = "Generating";
+  isImageGenerating = true;
+  
+
+  const imgCardMarkup = Array.from({ length: userImgQuantity }, () => 
+      `<div class="img-card loading">
+        <img src="images/loader.svg" alt="AI generated image">
+        <a class="download-btn" href="#">
+          <img src="images/download.svg" alt="download icon">
+        </a>
+      </div>`
+  ).join("");
+
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImages(userPrompt, userImgQuantity,userImgSize);
+}
+
+generateForm.addEventListener("submit", handleImageGeneration);
